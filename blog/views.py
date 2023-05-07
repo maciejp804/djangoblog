60799b63c19e351276118724760bf565dd5d9ada
maciejp804 <maciej.puchalski@onet.eu>
2023-05-07 17:24:41 +0200

    Dodanie szablonów

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -7,3 +7,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+#def post_detail
